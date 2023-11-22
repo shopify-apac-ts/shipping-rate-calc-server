31870a8306d9c9ffa057435b26613d90ba221563
Nobu Hayashi <nobu.hayashi@shopify.com>
2023-11-22 12:15:00 +0900

    Debugging /callback - loader() is called?

--- app/routes/callback.jsx
+++ app/routes/callback.jsx
@@ -32,9 +32,13 @@ export async function action({request, context}) {
   return json({shippingRates}, {status: 200});
 }
 
-export async function loader({context}) {
+export async function loader({request, context}) {
 
   console.log("/callback LOADER: context", context);
+
+  const body = await request.formData();
+  console.log("/callback ACTION: request", request);
+  console.log("body", body);
 
   const loaderResponse = {"message": "loader handled this request"};
   return json(loaderResponse, {status: 200});
