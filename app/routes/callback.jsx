98c580b6497ecd42622f0b803ed3a8f24aa414b1
Nobu Hayashi <nobu.hayashi@shopify.com>
2023-11-22 13:22:03 +0900

    Debugging /callback - implement rates in action() 2

--- app/routes/callback.jsx
+++ app/routes/callback.jsx
@@ -33,5 +33,25 @@ export async function action({request, context}) {
 
 export async function loader({context}) {
   console.log("/callback LOADER: context", context);
-  return json({});
+
+  const rates = [ 
+    { 
+      service_name: "canadapost-overnight", 
+      service_code: "ON", 
+      total_price: "129500", 
+      description: "This is the fastest option by far", 
+      currency: "JPY", 
+      min_delivery_date: "2023-12-01 14:48:45 +0900", 
+      max_delivery_date: "2023-12-10 14:48:45 +0900" 
+    }, { 
+      service_name: "fedex-2dayground", 
+      service_code: "2D", 
+      total_price: "293400", 
+      currency: "JPY", 
+      min_delivery_date: "2023-12-05 14:48:45 +0900", 
+      max_delivery_date: "2023-12-20 14:48:45 +0900" 
+    } 
+  ];
+
+  return json({rates}, {status: 200});
 }
