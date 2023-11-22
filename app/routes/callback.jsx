b1d6a7a0c80556eae021e169e753a57b57bc1fa1
Nobu Hayashi <nobu.hayashi@shopify.com>
2023-11-22 14:58:59 +0900

    Debugging /callback - implement rates in action() 13

--- app/routes/callback.jsx
+++ app/routes/callback.jsx
@@ -5,14 +5,12 @@ export async function action({request, context}) {
 
   console.log("/callback ACTION: context", context);
   console.log("/callback ACTION: request", request);
-
-//  const formData = await request.formData();
-//  console.log("data", formData);
   const requestJson = await request.json();
 //  console.log("requestJson", requestJson);
-  console.log("origin", JSON.stringify(requestJson.rate?.origin));
-  console.log("destination", JSON.stringify(requestJson.rate?.destination));
+//  console.log("origin", JSON.stringify(requestJson.rate?.origin));
+//  console.log("destination", JSON.stringify(requestJson.rate?.destination));
 //  console.log("items", JSON.stringify(requestJson.rate?.items));
+
   const items = Object.keys(requestJson.rate?.items).length;
   var total_price = 0;
   console.log("item_count", items);
@@ -21,12 +19,14 @@ export async function action({request, context}) {
     total_price += requestJson.rate?.items[i].price;
   }
   console.log("total_price", total_price);
+  const shippingPriceA = total_price * 0.2; // 20% of total_price
+  const shippingPriceB = total_price * 0.1; // 10% of total_price
 
   const rates = [ 
     { 
       service_name: "canadapost-air", 
       service_code: "ON", 
-      total_price: "129500", 
+      total_price: `${shippingPriceA}`, 
       description: "This is the fastest option by far", 
       currency: "JPY", 
       min_delivery_date: "2023-12-10 14:48:45 +0900", 
@@ -34,7 +34,7 @@ export async function action({request, context}) {
     }, { 
       service_name: "fedex-ground", 
       service_code: "2D", 
-      total_price: "293400", 
+      total_price: `${shippingPriceB}`, 
       currency: "JPY", 
       min_delivery_date: "2023-12-25 14:48:45 +0900", 
       max_delivery_date: "2024-01-10 14:48:45 +0900" 
